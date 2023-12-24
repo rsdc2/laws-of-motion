271778e50d0fa11932b1438db919ede5b57721f3
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-12-24 20:24:35 +0000

    something weird happening every other go

--- src/Pure/vector.js
+++ src/Pure/vector.js
@@ -24,5 +24,18 @@ const Vec = {
         const [r, θ] = Angle.toRθ(vec)
 
         return Angle.toVec(1, θ)
+    },
+
+    /**
+     * Substract vec2 from vec1
+     * @param {Vec2D} vec1 
+     * @param {Vec2D} vec2 
+     * @return {Vec2D}
+     */
+    sub: (vec1, vec2) => {
+        const [x1, y1] = vec1
+        const [x2, y2] = vec2
+
+        return [x1 - x2, y1 - y2]
     }
 }
--- src/SideEffects/timer.js
+++ src/SideEffects/timer.js
@@ -1,16 +1,41 @@
 let t = 0
 
+
 const Timer = {
     start: () => setInterval( () => {
         t += 1
 
-        const vec = Angle.toVec(15, 250)
+        const vel = Angle.toVec(5, 240)
 
-        Body.moveAtT(t, SOL1.xy, vec, [0, 0.1], sol1())
+        // const [x, y] = Vec.sub(Body.xy(sol1()), Body.xy(sol2()))
+        const [x, y] = Vec.sub(Body.xy(sol1()), SOL2.xy)
 
-        const [x, y] = Body.xy(sol1())
-        console.log(Angle.toRθ([x, y]))
+        // const [x1, y1] = Body.xy(sol1())
+        // const [x, y] = [x2 - x1, y1 - y2]
+        const [r, θ] = Angle.toRθ([x, y])
+        // const g = Motion.g(r, 1000000000000, [x, y])
+        // const [rg, rθ] = Angle.toRθ(g)
+        // console.log(x, y)
+        // console.log(x1, x2)
+        // console.log(r, θ)
 
-    }, 20)
+        // console.log(g, rg)
+        // console.log(g)
+        // console.log(θ, rθ)
+        if (x < -100) {
+            const g = Vec.toUnit([x, y])
+            Body.moveAtT(t, SOL1.xy, vel, g, sol1())
+            console.log(g, x,y)
+            
+            const [x2, y2] = Body.xy(sol1())
+        }
+        // console.log(Body.xy(sol1()))
+
+
+
+        // console.log(Angle.toRθ([x, y]))
+        // console.log()
+
+    }, 50)
 }
 

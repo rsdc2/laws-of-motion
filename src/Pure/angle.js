fec636ab4c0787c319699766afba2effdd40b2ae
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-01-07 07:26:54 +0000

    first commit for angle class

--- src/Pure/angle.js
+++ src/Pure/angle.js
@@ -1,18 +1,53 @@
 
-const Angle = {
+class Angle {
+
+    #angle
+    /**
+     * 
+     * @param {number} θ angle in degrees
+     */
+    constructor(θ) {
+        this.#angle = θ
+    }
+
+    get deg() {
+        return this.#angle
+    }
+
+    /**
+     * 
+     * @param {number} θ angle in degrees 
+     */
+    static fromDeg (θ) {
+        return new Angle(θ)
+    }
+
+    /**
+     * 
+     * @param {number} θ angle in radians 
+     * @returns 
+     */
+    static fromRad (θ) {
+        return new Angle(Angle.toRad(θ))
+    }
+
+    get rad() {
+        return Angle.toRad(this.#angle)
+    }
+ 
     /**
      * Converts radians to degrees
      * @param {number} rad angle in radians
      * @returns {number} angle in degrees
      */
-    toDeg: (rad) => (rad / (PI * 2)) * 360,
+    static toDeg = (rad) => (rad / (PI * 2)) * 360
 
     /**
      * Converts degrees to radians
      * @param {number} deg angle in degrees
      * @returns {number} angle in radians
      */
-    toRad: (deg) => (deg / 360) * PI * 2,
+    static toRad = (deg) => (deg / 360) * PI * 2
 
     /**
      * Takes a magnitude and an angle, and
@@ -21,12 +56,12 @@ const Angle = {
      * @param {number} θ angle
      * @returns {Vec2D} a 2D vector
      */
-    toVec: (r, θ) => {
+    static toVec = (r, θ) => {
         const x = r * cos(Angle.toRad(θ))
         const y = r * sin(Angle.toRad(θ))
 
         return [x, y]
-    },
+    }
 
     /**
      * Take a 2D vector, and return a magnitude
@@ -35,7 +70,7 @@ const Angle = {
      * @returns {[number, number]} a magnitude and 
      * an angle in degrees
      */
-    toPolar: vec => {
+    static toPolar = vec => {
         // cf. https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
         // cf. https://en.wikipedia.org/wiki/Atan2
         // cf. https://en.wikipedia.org/wiki/Polar_coordinate_system#Converting_between_polar_and_Cartesian_coordinates
@@ -45,6 +80,8 @@ const Angle = {
         return [r, θ]
     }
 
-
+    get value() {
+        return this.#angle
+    }
 }
 

252ce59d96ff0f65b770dfaebc835ce5b2b82976
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-01-22 19:46:42 +0000

    references for unit vector

--- src/Pure/unitvector.js
+++ src/Pure/unitvector.js
@@ -1,11 +1,8 @@
 
 /**
- * Class for handling 2D vectors.
- * Underlying interface is Vec2D, which is
- * a 2D array.
- * Any method with the suffix 'Vec' is used 
- * for interacting directly with Vec2D;
- * otherwise methods interact with Vector2D
+ * Class for handling 2D unit vectors.
+ * Contains a Vector which is scaled according 
+ * to the unit needed
  * 
  */
 
@@ -13,7 +10,9 @@
 class UnitVector {
 
     // cf. https://en.wikipedia.org/wiki/Unit_vector
-
+    // https://eng.libretexts.org/Bookshelves/Mechanical_Engineering/Engineering_Statics%3A_Open_and_Interactive_(Baker_and_Haynes)/02%3A_Forces_and_Other_Vectors/2.05%3A_Unit_Vectors
+    // https://www.cuemath.com/calculus/unit-vector/
+    
     #vector
     /**
      * 
